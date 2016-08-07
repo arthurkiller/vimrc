6796d9ae2c686e5e1741f4a10036b62281a8735a
arthur@lza <arthur-lee@qq.com / lza@meitu.com>
2016-08-07 18:02:24 +0800

    fast fix

--- install.sh
+++ install.sh
@@ -2,11 +2,19 @@
 # this shell is used to install the vimrc file to your home dir automaticly
 # for more information , please mail me at arthurkiller21@gmail.com
 trap exit ERR
-cp vimrc ../.vimrc
-cp vimrc.vundle ../.vimrc.vundle
+
+if [! -d ~/.vim]; then
+    rm -rf ~/.vim 
+fi
 cp -r vim ../.vim
 
-mkdir ~/.vim/bundle
+cp vimrc ../.vimrc
+cp vimrc.vundle ../.vimrc.vundle
+
+if [! -d ~/.vim/bundle]; then
+    mkdir ~/.vim/bundle
+fi
+
 cd ~/.vim/bundle 
 git clone https://github.com/gmarik/Vundle.vim.git
 git clone https://github.com/tpope/vim-fugitive.git
